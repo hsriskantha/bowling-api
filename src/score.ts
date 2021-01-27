cf20710f6a934fa24a0ee6b29628afbbc83570e9
Hari Sriskantha <haris@theodo.co.uk>
2021-01-27 10:22:23 +0000

    Add more hints

--- src/score.ts
+++ src/score.ts
@@ -1,5 +1,12 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { DynamoDB } from 'aws-sdk';
+
 import 'source-map-support/register';
+
+const documentClient = new DynamoDB.DocumentClient({
+  region: 'localhost',
+  endpoint: 'http://localhost:8000'
+});
 
 export const update: APIGatewayProxyHandler = async (event, _context) => {
   return {
